8eae083265815c5b8b6d5f90c2045c0ce63b5593
Alex White <alexwhite@unknown542696db2f5b.gateway.2wire.net>
2013-10-11 17:26:39 -0500

    Initial Commit

---
+++ Super Tic Tac Toe/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Super Tic Tac Toe
+//
+//  Created by Alex White on 10/11/13.
+//  Copyright (c) 2013 Alex White. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Super Tic Tac Toe/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Super Tic Tac Toe
+//
+//  Created by Alex White on 10/11/13.
+//  Copyright (c) 2013 Alex White. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
